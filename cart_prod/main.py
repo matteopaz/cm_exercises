03fe4456dd43063e955a51b70775ba7f58556573
matteopaz <matteopi314@gmail.com>
2022-08-18 22:20:03 +0000

    cart prod done!

--- cart_prod/main.py
+++ cart_prod/main.py
@@ -1,24 +1,19 @@
-def copy(arr):
-    newarr = []
-    for e in arr:
-        newarr.append(e)
-    return newarr
+# def copy(arr):
+#     newarr = []
+#     for e in arr:
+#         newarr.append(e)
+#     return newarr
 
 def cartesian_product(ranges):
-    order = 1
+    points = [[]]
     for range in ranges:
-        order = order * len(range)
-    pts = []
+        extended_pts = []
 
-    for i in range(order):
-        pts.append([])
-    
-    for range in ranges:
-        points = copy(pts)
-        for val in range:
-            for point in points:
-                point.append(val)
-        pts = points
-    return pts
+        for val in range: 
+            for pt in points:
+                extended_pts.append(pt+[val])
 
-print(cartesian_product([[0,1],[2,3]]))
+        points = extended_pts
+    return points
+
+print(cartesian_product([["a", "b", "c","d","e","f","g"],range(5)]))
