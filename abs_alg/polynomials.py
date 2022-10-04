7dc5baf51a4101258c34995373372166c8bdc598
Matteo Paz <86904497+matteopaz@users.noreply.github.com>
2022-10-03 23:15:32 -0700

    poly done

--- abs_alg/polynomials.py
+++ abs_alg/polynomials.py
@@ -1,12 +1,10 @@
-# class MultiPolynomial:
-#     def __init__(self, order, coeffs):
-#         self.order = order
-#         self.coeffs = coeffs
+import matplotlib.pyplot as plt
 
 class Polynomial:
     def __init__(self, order, coeffs):
-        if len(coeffs) != order:
+        if len(coeffs) != order + 1:
             raise ValueError("Invalid number of coefficients")
+        self.num_coeffs = order + 1
         self.order = order
         self.coeffs = coeffs
     
@@ -14,9 +12,9 @@ class Polynomial:
     def eval(self, x):
         val = 0
         ascending_coeffs = self.get_coefficients()[::-1]
-        for deg in range(self.order):
+        for deg in range(self.num_coeffs):
             val += x**deg * ascending_coeffs[deg]
-            print(f"deg: {deg}, co: {ascending_coeffs[deg]},  val: {val}")
+            # print(f"deg: {deg}, co: {ascending_coeffs[deg]},  val: {val}")
         return val
 
     def get_coefficients(self):
@@ -36,7 +34,7 @@ class Polynomial:
             return Polynomial(1, [0])
 
         ascending_coeffs = __poly.get_coefficients()[::-1]
-        for deg in range(__poly.get_order()):
+        for deg in range(__poly.num_coeffs):
             ascending_coeffs[deg] *= deg
         coeffs = ascending_coeffs[::-1]
         new = Polynomial(__poly.get_order() - 1, coeffs[:-1])
@@ -48,43 +46,42 @@ class Polynomial:
 
         self_coeffs = self.get_coefficients()
         poly_coeffs = poly.get_coefficients()
-        for deg in range(poly.get_order()):
+        for deg in range(poly.num_coeffs):
             self_coeffs[deg] += poly_coeffs[deg]
         return Polynomial(self.get_order(), self_coeffs)
     
     def multiply(self, poly):
         self_coeffs = self.get_coefficients()
         poly_coeffs = poly.get_coefficients()
-        self_order = self.get_order()
-        poly_order = poly.get_order()
 
-        coeffs = [0] * (self_order + poly_order - 1)
-        for deg in range(self.get_order()):
-            for deg2 in range(poly.get_order()):
+        coeffs = [0] * (self.num_coeffs + poly.num_coeffs - 1)
+        for deg in range(self.num_coeffs):
+            for deg2 in range(poly.num_coeffs):
                 coeffs[deg + deg2] += self_coeffs[deg] * poly_coeffs[deg2]
         return Polynomial(self.get_order() + poly.get_order() - 1, coeffs)
     
     def __recursive_newton(self, x, acc):
-        tan_root = x - self.eval(x) / self.get_derivative(1).eval(x)
+        tan_root = x - (self.eval(x) / self.get_derivative(1).eval(x))
         
         if x - tan_root < 10**(-1*acc):
             roundable = tan_root * 10**(acc)
             rounded = round(roundable) / 10**(acc)
             return rounded
+        if abs(self.get_derivative(1).eval(x)) > 10**8:
+            return False
 
         return self.__recursive_newton(tan_root, acc)
 
-    def roots(self):
-        return self.__recursive_newton(1, 5)
+    def root(self, initial_guess=5, acc=5):
+        return self.__recursive_newton(initial_guess, acc)
         
 
     def __str__(self):
         coeffs = self.get_coefficients()
-        order = self.get_order()
 
         s = ""
-        for i in range(order):
-            deg = order - i - 1
+        for i in range(self.num_coeffs):
+            deg = self.num_coeffs - i - 1
             if coeffs[i] == 0:
                 if deg == 0:
                     s += "  0"
@@ -101,8 +98,19 @@ class Polynomial:
     def display(self):
         print(self.__str__())
         return self
+    
+    def graph(self, pyplot, x_range, res):
+        x = []
+        y = []
+        # append to x and y with res points per unit
+        for i in range(x_range[0] * res, x_range[1] * res):
+            x.append(i / res)
+            y.append(self.eval(i / res))
+        
+        pyplot.plot(x, y)
+        pyplot.show()
+        return self
 
-p = Polynomial(3, [1,-2,1])
-n = Polynomial(3, [1,2,1])
-d = p.multiply(n).get_derivative(1).display()
-print(d.eval(3))
+
+poly = Polynomial(3, [1, -2, -1, 0])
+poly.graph(plt, [-3, 3], 100)
