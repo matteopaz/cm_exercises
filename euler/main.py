4f2f456111d4501068d1763eadbe810f3563f979
matteopaz <86904497+matteopaz@users.noreply.github.com>
2022-10-07 15:29:05 -0700

    SIR

--- euler/main.py
+++ euler/main.py
@@ -1,6 +1,4 @@
-
-
-
+import matplotlib.pyplot as plt
 class EulerEstimator:
     def __init__(self, derivatives):
         self.df = derivatives
@@ -31,26 +29,48 @@ class EulerEstimator:
             t += step_size
         return history
 
-initial_state = {"a": -0.45, "b": -0.05, "c": 0}
-def da_dt(t, state):
-    return state["a"] + 1
+initial_state = {"S": 1000, "I": 1, "R": 0}
 
-def db_dt(t, state):
-    return state["b"] + state["a"]
+def dS_dt(t, state):
+    return state["S"] * -0.01 * state["I"] * 0.03
 
-def dc_dt(t, state):
-    return 2 * state["b"] + 3 * t
+def dI_dt(t, state):
+    return  -dS_dt(t, state) - dR_dt(t, state)
 
-derivatives = {"a": da_dt, "b": db_dt, "c": dc_dt}
+def dR_dt(t, state):
+    return 0.02*state["I"]
+
+derivatives = {"S": dS_dt, "I": dI_dt, "R": dR_dt}
 estimator = EulerEstimator(derivatives)
 
-initial_pt = (-0.4, initial_state)
+initial_pt = (0, initial_state)
 
-step_size = 2
-num_steps = 3
+step_size = 0.1
+num_steps = 1000
 
 der = estimator.eval_derivative(initial_pt)
 ests = estimator.estimate(initial_pt, step_size, num_steps)
 
 print(der)
-print(ests)
+# print(ests)
+
+
+def plot(ests):
+    t = []
+    S = []
+    I = []
+    R = []
+    for est in ests:
+        t.append(est[0])
+        S.append(est[1]["S"])
+        I.append(est[1]["I"])
+        R.append(est[1]["R"])
+
+    # color theme blue, red, green
+    plt.plot(t, S, label="S", color="#1f77b4")
+    plt.plot(t, I, label="I", color="#ff7f0e")
+    plt.plot(t, R, label="R", color="#2ca02c")
+    plt.legend()
+    plt.show()
+
+plot(ests)
