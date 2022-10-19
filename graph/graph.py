607e422cfe0d6243a6b925a785edb05084cc7088
matteopaz <86904497+matteopaz@users.noreply.github.com>
2022-10-19 15:13:08 -0700

    graphs done

--- graph/graph.py
+++ graph/graph.py
@@ -43,7 +43,7 @@ class Node:
     def __init__(self, id):
         self.id = id
         self.previous = None
-        self.distance = 0
+        self.distance = -1
         self.children = set()
         self.parents = set()
     
@@ -71,7 +71,7 @@ class Graph:
             if origin.id not in self.nodes:
                 self.nodes[origin.id] = origin
             if end.id not in self.nodes:
-                self.node[end.id] = end
+                self.nodes[end.id] = end
         
         for edge in edges:
             originid = edge[0]
@@ -85,11 +85,11 @@ class Graph:
     def node(self, id):
         return self.nodes[id]
 
-    # def get_dict(self):
-    #     return self.dict.copy()
+    def get_children(self,node):
+        return self.nodes[node].getchildren()
     
-    # def get_reversed_dict(self):
-    #     return self.reversed_dict.copy()
+    def get_parents(self, node):
+        return self.nodes[node].getparents()
 
     def get_ids_breadth_first(self, root):
         queue = Queue([root])
@@ -112,9 +112,9 @@ class Graph:
         traversal = []
 
         while stack.len() != 0:
-            node = stack.unload()
-            traversal.append(node)
-            children = self.get_children(node)
+            node = self.nodes[stack.unload()]
+            traversal.append(node.id)
+            children = node.getchildren()
             for child in children:
                 if child not in visited:
                     stack.load(child)
@@ -124,58 +124,80 @@ class Graph:
     def set_distance_and_previous(self, root):
         queue = Queue([root])
         visited = {root: True}
-        root.distance = 0
-        root.previous = None
+        self.nodes[root].distance = 0
+        self.nodes[root].previous = None
+
+        for node in self.nodes:
+            self.nodes[node].previous = None
+            self.nodes[node].distance = -1
 
         while queue.len() != 0:
-            node = queue.unload()
-            children = self.get_children(node)
-            for child in children:
+            node = self.nodes[queue.unload()]
+            for child in node.getchildren():
                 if child not in visited:
                     queue.load(child)
                     visited[child] = True
-                    child.distance = node.distance + 1
-                    child.previous = node
+                    self.nodes[child].distance = node.distance + 1
+                    self.nodes[child].previous = node.id
     
     def calc_distance(self, origin, end):
         self.set_distance_and_previous(origin)
-        return self.dict[end].distance
+        return self.nodes[end].distance + 1
     
     def shortest_path(self, origin, end):
         self.set_distance_and_previous(origin)
         path = []
-        current_node = end
+        current_node = self.nodes[end]
+        prev = current_node.previous
+        while prev != origin:
+            if not prev:
+                return False
+            path.append(prev)
+            current_node = self.nodes[prev]
+            prev = current_node.previous
+        return [origin] + path[::-1] + [end]
+
+
+
+edges = [
+    (0, 1), (0,2),
+    (1,3),
+    (2,3), (2,5),
+    (3,1), (3,4),
+    (6,5), (6,7),
+    (7,6), (7,8),
+    (8,9), (8,7),
+    (9,8)
+]
 
 g = Graph(edges)
-print(g.get_dict())
-print(g.get_children(3))
-print(g.get_parents(3))
-print(g.get_ids_breadth_first(4))
-print(g.get_ids_depth_first(4))
-
-edges2 = [
-    (0,1), (1,2), (2,3), (3,0)
-]
-g2 = Graph(edges2)
-print(g2.get_dict())
-print(g2.get_ids_breadth_first(0))
-print(g2.get_ids_depth_first(0))
 
 
-edges3 = [
-    (0,1), (1,2), (2,3), (3,0),
-    (4,5), (5,6), (6,7), (7,4)
-]
-g3 = Graph(edges3)
-print(g3.get_dict())
-print(g3.get_ids_breadth_first(0))
+print(g.calc_distance(0, 4))
+print(g.shortest_path(0, 4))
+# edges2 = [
+#     (0,1), (1,2), (2,3), (3,0)
+# ]
+# g2 = Graph(edges2)
+# print(g2.get_dict())
+# print(g2.get_ids_breadth_first(0))
+# print(g2.get_ids_depth_first(0))
 
-print(g3.get_ids_breadth_first(4))
 
-edges4 = []
-for i in range(1):
-    edges4.append((i, i+1))
-g4 = Graph(edges4)
-print(g4.get_dict())
-print(g4.get_ids_breadth_first(0))
+# edges3 = [
+#     (0,1), (1,2), (2,3), (3,0),
+#     (4,5), (5,6), (6,7), (7,4)
+# ]
+# g3 = Graph(edges3)
+# print(g3.get_dict())
+# print(g3.get_ids_breadth_first(0))
+
+# print(g3.get_ids_breadth_first(4))
+
+# edges4 = []
+# for i in range(1):
+#     edges4.append((i, i+1))
+# g4 = Graph(edges4)
+# print(g4.get_dict())
+# print(g4.get_ids_breadth_first(0))
 
